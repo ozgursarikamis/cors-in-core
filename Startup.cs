60a1c393949492e9fa747b255912f401efac99b9
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-26 16:43:02 +0300

    access-control-expose-headers

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,10 @@ namespace CorsInCore
             services.AddCors(options =>
                 {
                     options.AddPolicy("AllowEverything", corsPolicyBuilder =>
-                        corsPolicyBuilder.WithOrigins(allowedOrigins).AllowCredentials());
+                    {
+                        corsPolicyBuilder.WithOrigins(allowedOrigins).AllowCredentials();
+                        corsPolicyBuilder.WithExposedHeaders("PageNo", "PageSize", "PageCount", "PageTotalRecords");
+                    });
 
                     options.AddPolicy("PublicApi", builder => 
                         builder.AllowAnyOrigin()
